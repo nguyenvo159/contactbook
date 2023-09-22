5c30099f80756c24fe19861f12705520689fee9c
TUF <nguyenvo1373.hg@gmail.com>
2023-09-22 10:59:50 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -23,9 +23,6 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    // Middleware xử lý lỗi tập trung.
-    // Trong các đoạn code xử lý ở các route, gọi next(error)
-    // sẽ chuyển về middleware xử lý lỗi này
     return res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
     });
